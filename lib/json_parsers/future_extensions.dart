8ec67ceeea27f51bc4f49aa7b00f676fc8342405
ShohidulProgrammer <shoikat.cse@gmail.com>
2025-08-03 22:11:18 +0600

    Added two-way sync script

--- lib/json_parsers/future_extensions.dart
+++ lib/json_parsers/future_extensions.dart
@@ -1,6 +1,7 @@
 extension FutureListOfMapsExtension on Future<List<Map<String, dynamic>?>?> {
   Future<List<T>> mapToNonNullableModelList<T>(
-      T Function(Map<String, dynamic>) fromJson) async {
+    T Function(Map<String, dynamic>) fromJson,
+  ) async {
     final list = await this;
     return list
             ?.whereType<Map<String, dynamic>>()
--- lib/json_parsers/stream_extensions.dart
+++ lib/json_parsers/stream_extensions.dart
@@ -3,12 +3,15 @@
 extension StreamOfListOfMapsExtension<T>
     on Stream<List<Map<String, dynamic>?>?> {
   Stream<List<T>>? mapToNonNullableModelStream<T>(
-      T Function(Map<String, dynamic>) fromJson) {
-    return map((list) =>
-        list
-            ?.whereType<Map<String, dynamic>>()
-            .map((json) => fromJson(json))
-            .toList() ??
-        []);
+    T Function(Map<String, dynamic>) fromJson,
+  ) {
+    return map(
+      (list) =>
+          list
+              ?.whereType<Map<String, dynamic>>()
+              .map((json) => fromJson(json))
+              .toList() ??
+          [],
+    );
   }
 }
